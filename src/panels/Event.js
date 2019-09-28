058773c843dc0a3982c65ae061911027d35b0605
Liubov Stepanova <step-li@yandex-team.ru>
2019-09-29 01:34:38 +0300

    feat: add register to event + get data from db

--- src/panels/Event.js
+++ src/panels/Event.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 
-import { platform, IOS, Group,
-    Cell, Button, Avatar,
-    InfoRow, List,
+import {
+    platform, IOS, Group,
+    Cell, Button, Avatar, Div,
+    InfoRow, List,  Link,
     FormLayoutGroup, Checkbox, FormLayout,
     HeaderButton, Panel, PanelHeader
 } from '@vkontakte/vkui';
@@ -14,7 +15,19 @@ const Event = ({ event, go, id, register }) => {
     const [eventData, setEventData] = useState({});
 
     useEffect(() => {
-        setEventData(JSON.parse(event));
+        async function getEventDBData(event) {
+            const resp = await fetch(`https://demo11.alpha.vkhackathon.com:433/api/events/getEvent?auth=oX5n!E2i.VpWpHeo8E6F0q&id=${event.eventId}`, {
+                mode: "cors"
+            });
+
+            const eventDB = await resp.json();
+
+            setEventData({ VK: event, ...eventDB });
+        }
+
+        const eventShort = JSON.parse(event);
+        getEventDBData(eventShort);
+
     }, [event]);
 
     return (
@@ -25,37 +38,61 @@ const Event = ({ event, go, id, register }) => {
                 </HeaderButton>}
             >
                 {eventData.active ? 'Запись на мероприятие' : "Описание мероприятия"}
-        </PanelHeader>
-            <Group>
+            </PanelHeader>
+            {JSON.stringify(eventData)}
+            {eventData.VK && eventData.VK.id && <Group>
                 <Cell
-                    photo={eventData.photo_200}
+                    photo={eventData.VK.photo_200}
                     //todo через start_date
-                    description={eventData.activity}
-                    //<Link href={`https://vk.com/club${eventData.id}`}><Button>Группа мероприятия</Button></Link>
-                    bottomContent={eventData.description}
-                    before={<Avatar src={eventData.photo_200} size={80} />}
+                    description={eventData.VK.activity}
+                    bottomContent={eventData.VK.description}
+                    asideContent={<Link href={`https//vk.com/app${eventData.VK.id}`}><Button>В группу</Button></Link>}
+                    before={<Avatar src={eventData.VK.photo_200} size={56} />}
                     size="l"
                 >
-                    {eventData.name}
+                    {eventData.VK.name}
                 </Cell>
-            </Group>
+            </Group>}
             <Group>
                 <List>
+                    <Cell multiline>
+                        <InfoRow title="Название">
+                            {eventData.name}
+                        </InfoRow></Cell>
                     <Cell multiline>
                         <InfoRow title="Описание">
                             {eventData.description}
                         </InfoRow></Cell>
                     <Cell multiline>
+                        <InfoRow title="Дата проведения">
+                            {eventData.date}
+                        </InfoRow></Cell>
+                    <Cell multiline>
+                        <InfoRow title="Место проведения">
+                            {eventData.place}
+                        </InfoRow></Cell>
+                    <Cell multiline>
                         <InfoRow title="Задачи волонтеров">
-                            {eventData.description}
+                            {eventData.volunteers_task}
                         </InfoRow>
                     </Cell>
-                    {eventData.active && <FormLayout>
+                    <Cell multiline>
+                        <InfoRow title="Требования к волонтерам">
+                            {eventData.volunteer_requirements}
+                        </InfoRow>
+                    </Cell>
+                    {eventData.VK && eventData.VK.active && <FormLayout>
                         <FormLayoutGroup top="Выберите удобное время">
-                            <Checkbox>15:00 - 18:00</Checkbox>
-                            <Checkbox>18:00 - 21:00</Checkbox>
+                            { eventData.time_periods && eventData.time_periods.map(period =>  {
+                                const isAvailable = period.is_available == 'true';
+                                return isAvailable? (
+                                    <Checkbox>{period.time_period}</Checkbox>
+                                ) : (
+                                    <Div>{period.time_period}</Div>
+                                );
+                            })}
                         </FormLayoutGroup>
-                        <Button onClick={register} data-id={eventData.id} >Записаться</Button>
+                        <Button onClick={register} data-period_ids={'1,2,3'} data-event_id={eventData.VK.id}>Записаться</Button>
                     </FormLayout>}
 
                 </List>
