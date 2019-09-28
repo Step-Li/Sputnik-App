b7ab9148ea1201dcca188adef7d08de217716498
Vutya <viktor-timohov@mail.ru>
2019-09-28 21:49:26 +0300

    fix: questionnaire changed

--- src/panels/Questionnaire.js
+++ src/panels/Questionnaire.js
@@ -19,8 +19,24 @@ import connect from "@vkontakte/vk-connect";
 const osName = platform();
 
 const Questionnaire = ({id, go, data}) => {
+    const user = {
+        last_name: data.last_name, first_name: data.first_name, father_name: '',
+        bdate: data.bdate, sex: data.sex,
+        email: '', phone: '',
+        occupation: '', specialty: '', languages: '',
+        volExp: '', childExp: '', extraSkills: '',
+        expects: '', medContrad: '', foodPref: '',
+        infoSource: '', size: ''
+    };
     const [phone, setPhone] = useState('');
     const [email, setEmail] = useState('');
+    const texts = [{name: 'volExp', text: 'Опыт волонтёрской деятельности'},
+        {name: 'childExp',text: 'Опыт работы с детьми'},
+        {name: 'extraSkills',text: 'Дополнительные навыки'},
+        {name: 'expects',text: 'Ожидания'},
+        {name: 'medContrad',text: 'Медицинские противопоказания'},
+        {name: 'foodPref',text: 'Предпочтения в еде'},
+        {name: 'infoSource',text: 'Откуда Вы о нас узнали?'}];
 
     useEffect(() => {
         async function fetchData() {
@@ -32,6 +48,15 @@ const Questionnaire = ({id, go, data}) => {
         fetchData();
     }, []);
 
+    const handleClick = () => {
+
+    };
+
+    const onChange = (e) => {
+        const { name, value } = e.currentTarget;
+        user[name] = value;
+    };
+
     return (
             <Panel id={id} theme="white">
                 <PanelHeader
@@ -41,47 +66,68 @@ const Questionnaire = ({id, go, data}) => {
                 >
                     Анкета волонтёра
                 </PanelHeader>
-                <FormLayout>
+                <FormLayout name='new-volunteer'>
                     <Input top="Фамилия" name='last_name'
-                           defaultValue={data.last_name}
+                           onChange={() => onChange()}
+                           value={user.last_name}
                     />
                     <Input top="Имя" name='first_name'
-                           defaultValue={data.first_name}
+                           onChange={() => onChange()}
+                           value={user.first_name}
                     />
-                    <Input top="Отчество" name='first_name' />
-                    <Input top="Дата рождения" name='birth_date'
-                           defaultValue={data.bdate}
+                    <Input top="Отчество" name='father_name'
+                           onChange={() => onChange()}
+                           value={user.father_name}
+                    />
+                    <Input top="Дата рождения" name='bdate'
+                           onChange={() => onChange()}
+                           value={user.bdate}
                     />
 
-                    <Select top="Пол" placeholder="Выберите пол"
-                            defaultValue={data.sex === 2 ? 'm' :(data.sex === 1 ? 'f' : null)}
+                    <Select top="Пол" placeholder="Выберите пол" name="sex"
+                            value={user.sex === 2 ? 'm' :(user.sex === 1 ? 'f' : null)}
+                            onChange={() => onChange()}
                     >
                         <option value="m">Мужской</option>
                         <option value="f">Женский</option>
                     </Select>
 
-                    <Input
-                        type="email"
-                        top="E-mail"
-                        name="email"
-                        defaultValue={email.email}
+                    <Input type="email" top="E-mail" name="email"
+                           onChange={() => onChange()}
+                           name="email" value={email.email}
                     />
 
-                    <Input top='Телефон' defaultValue={phone.phone_number}/>
+                    <Input top='Телефон' name="phone"
+                           onChange={() => onChange()}
+                           value={phone.phone_number}
+                    />
 
-                    <Input top='Место учёбы/работы'/>
-                    <Input top='Специальность'/>
-                    <Input top='Языки' />
+                    <Input top='Место учёбы/работы' name="occupation"
+                           onChange={() => onChange()}
+                           value={user.occupation}
+                    />
+                    <Input top='Специальность' name="specialty"
+                           onChange={() => onChange()}
+                           value={user.specialty}
+                    />
+                    <Input top='Языки' name="languages"
+                           onChange={() => onChange()}
+                           value={user.languages}
+                    />
 
-                    <Textarea top='Опыт волонтёрской деятельности'/>
-                    <Textarea top='Опыт работы с детьми'/>
-                    <Textarea top='Дополнительные навыки'/>
-                    <Textarea top='Ожидания'/>
-                    <Textarea top='Медицинские противопоказания'/>
-                    <Textarea top='Предпочтения в еде'/>
-                    <Textarea top='Откуда Вы о нас узнали?' />
+                    {texts.map(({name, text}) => (
+                        <Textarea name={name} key={name} top={text}
+                                  onChange={() => onChange()}
+                                  value={user[name]}
+                        />
+                    ))}
 
-                    <Select top="Размер одежды" placeholder="Выберите размер одежды">
+
+
+                    <Select top="Размер одежды" placeholder="Выберите размер одежды"
+                            onChange={() => onChange()}
+                            value={user.size}
+                    >
                         <option value="xs">XS</option>
                         <option value="s">S</option>
                         <option value="m">M</option>
@@ -92,7 +138,7 @@ const Questionnaire = ({id, go, data}) => {
 
                     <Checkbox>Согласны ли Вы на получение рассылки?</Checkbox>
 
-                    <Button size="xl">Отправить анкету</Button>
+                    <Button size="xl" onClick={handleClick()}>Отправить анкету</Button>
                 </FormLayout>
             </Panel>
         );
