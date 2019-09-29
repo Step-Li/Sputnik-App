e72709b32042a88e7f420dc2d35ba0ae75109c7e
Liubov Stepanova <step-li@yandex-team.ru>
2019-09-29 07:43:07 +0300

    feat: add send new event method

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState, useEffect } from 'react';
 import connect from '@vkontakte/vk-connect';
 import '@vkontakte/vkui/dist/vkui.css';
 import { View, Alert } from '@vkontakte/vkui';
@@ -8,8 +8,9 @@ import Questionnaire from "./panels/Questionnaire";
 import Home from './panels/Home';
 import Event from './panels/Event';
 import EventForm from './panels/EventForm';
-import Modal from  './components/Modal';
+import Modal from './components/Modal';
 import TaskForm from './panels/TaskForm';
+import OrgEvents from './panels/OrgEvents';
 
 const App = () => {
 	const [activePanel, setActivePanel] = useState('home');
@@ -22,7 +23,7 @@ const App = () => {
 	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
 
 	useEffect(() => {
-		connect.subscribe(({ detail: { type, data }}) => {
+		connect.subscribe(({ detail: { type, data } }) => {
 			if (type === 'VKWebAppUpdateConfig') {
 				const schemeAttribute = document.createAttribute('scheme');
 				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
@@ -30,9 +31,9 @@ const App = () => {
 			}
 		});
 		async function fetchData() {
-			const user = await connect.sendPromise('VKWebAppGetUserInfo');	
-			const token = await connect.sendPromise("VKWebAppGetAuthToken", {"app_id": 7150436, "scope": "groups"});	
-			setUser({...user, admin: true});
+			const user = await connect.sendPromise('VKWebAppGetUserInfo');
+			const token = await connect.sendPromise("VKWebAppGetAuthToken", { "app_id": 7150436, "scope": "groups" });
+			setUser({ ...user, admin: true });
 			setToken(token.access_token);
 			setPopout(null);
 		}
@@ -54,11 +55,11 @@ const App = () => {
 	const showPopout = (message) => {
 		setPopout(
 			<Alert
-			actionsLayout="vertical"
-			onClose={closePopout}
-		  >
-			<h2>{message}</h2>
-		  </Alert>
+				actionsLayout="vertical"
+				onClose={closePopout}
+			>
+				<h2>{message}</h2>
+			</Alert>
 		);
 	};
 
@@ -72,8 +73,8 @@ const App = () => {
 		&user_vk_id=${fetchedUser.id}
 		&event_id=${e.currentTarget.dataset.event_id}
 		&time_period_ids=${e.currentTarget.dataset.period_ids}`, {
-			mode: "cors"
-		});
+				mode: "cors"
+			});
 		const status = await res.json();
 		if (status.success) {
 			showPopout("Вы успешно зарегистрировались");
@@ -99,14 +100,20 @@ const App = () => {
 	}
 
 	return (
-		<View activePanel={activePanel} popout={popout} 
+		<View activePanel={activePanel} popout={popout}
 			modal={
 				<Modal token={token} closeModal={closeModal} activeModalId={activeModalPage}></Modal>
 			}>
 			<Home id='home' go={go} alert={alert} fetchedUser={fetchedUser} token={token} />
 			<Questionnaire id='new-user' go={go} data={fetchedUser} alert={showPopout} />
 			<Event id='event' event={openedEvent} go={go} register={register} />
-			<EventForm id='event-form' go={go} periodsList={periods} openModal={openModal} clearSelectedEvent={clearSelectedEvent} selectedEventJSON={selectedEvent} />
+			<EventForm id='event-form'
+				go={go}
+				periodsList={periods}
+				openModal={openModal}
+				clearSelectedEvent={clearSelectedEvent}
+				selectedEventJSON={selectedEvent}
+				userId={fetchedUser && fetchedUser.id}/>
 			<TaskForm id='task-form' go={go} />
 			<OrgEvents id='org-events' go={go} user={fetchedUser} ></OrgEvents>
 		</View>
