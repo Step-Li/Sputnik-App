deed5af5ad0501bebf6293fba49373e7ced48a46
Scuuter <class.object@yandex.ru>
2019-09-29 05:09:39 +0300

    improve buttons in EventForm

--- src/panels/EventForm.js
+++ src/panels/EventForm.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
+import Icon16Add from '@vkontakte/icons/dist/16/add';
+import Icon16Cancel from '@vkontakte/icons/dist/16/cancel';
 
 import {
     platform, IOS, FormLayout,
@@ -32,7 +34,7 @@ const EventForm = ({ go, id, openModal, selectedEventJSON, periodsList, clearSel
                     before={<Avatar src={selectedEvent.photo_100} size={56} />}
                     size="xl"
                     description={selectedEvent.activity}
-                    asideContent={<Button onClick={clearSelectedEvent} >No</Button>}
+                    asideContent={<Button before={<Icon16Cancel/>} level="destructive" onClick={clearSelectedEvent}>Удалить</Button>}
                 >
                     {selectedEvent.name}</Cell>}
                 <CellButton onClick={openModal} data-modal_id='select-event'>{selectedEvent ? 'Изменить' : 'Привязать мероприятие'}</CellButton>
@@ -42,7 +44,8 @@ const EventForm = ({ go, id, openModal, selectedEventJSON, periodsList, clearSel
                     <Textarea top="Задачи волонтеров"></Textarea>
                     <FormLayoutGroup top="Временные промежутки">
                         {JSON.stringify(periods)}
-                        <CellButton onClick={openModal} data-modal_id='add-time-period'>Добавить временной промежуток</CellButton>
+                        <CellButton before={<Icon16Add/>} onClick={openModal} data-modal_id='add-time-period'>Добавить
+                            временной промежуток</CellButton>
                     </FormLayoutGroup>
                 </FormLayout>
             </Group>
